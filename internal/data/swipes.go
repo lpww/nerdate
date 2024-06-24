e153fdeb11d9aeb17d10ecf9d627e7b17a3e41a4
thomas <thomas.treffry@nearform.com>
2024-06-24 17:39:18 +0100

    feat: add json tags and Equal comparison to swipes

--- internal/data/swipes.go
+++ internal/data/swipes.go
@@ -3,11 +3,15 @@ package data
 import "time"
 
 type Swipe struct {
-	ID int64
+	ID int64 `json:"id"`
 
-	UserID       int64 // todo: use the user.ID field when it's been created
-	Liked        bool
-	SwipedUserID int64 // todo: use the user.ID field when it's been created
+	UserID       int64 `json:"user_id"` // todo: use the user.ID field when it's been created
+	Liked        bool  `json:"liked"`
+	SwipedUserID int64 `json:"swiped_user_id"` // todo: use the user.ID field when it's been created
 
-	CreatedAt time.Time
+	CreatedAt time.Time `json:"-"`
+}
+
+func (s1 Swipe) Equal(s2 Swipe) bool {
+	return s1.UserID == s2.UserID && s1.Liked == s2.Liked && s1.SwipedUserID == s2.SwipedUserID
 }
