f58382fe0b154a6cbf24896439e2f59871485815
thomas <thomas.treffry@nearform.com>
2024-06-24 17:38:49 +0100

    chore: remove unused helper

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -6,22 +6,8 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"strconv"
 	"strings"
-
-	"github.com/julienschmidt/httprouter"
 )
-
-func (app *application) readIDParam(r *http.Request) (int64, error) {
-	params := httprouter.ParamsFromContext(r.Context())
-
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
-		return 0, errors.New("invalid id parameter")
-	}
-
-	return id, nil
-}
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
 	maxBytes := 1_048_576 // 1MB
