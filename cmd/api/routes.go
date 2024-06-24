e1ae1c9d651ee00cf55b9b0e1e3d39bbe77bdc8a
thomas <thomas.treffry@nearform.com>
2024-06-24 16:58:00 +0100

    feat: add error handling to routes

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,7 +9,11 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/swipe", app.createSwipeHandler)
 
 	return router
 }
