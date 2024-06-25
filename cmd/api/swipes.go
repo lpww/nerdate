457f44b8f7a47b7c99193b455d1e75c13c2f0a79
thomas <thomas.treffry@nearform.com>
2024-06-25 10:03:25 +0100

    feat: process create swipe handler input

--- cmd/api/swipes.go
+++ cmd/api/swipes.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func (app *application) createSwipeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new swipe")
+	var input struct {
+		SwipedUserID int64 `json:"swiped_user_id"`
+		Liked        bool
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
