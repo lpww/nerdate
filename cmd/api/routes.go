cea0b5a61aceaa45eff6673ec2321ddf1fc43ed3
thomas <thomas.treffry@nearform.com>
2024-07-01 14:07:48 +0100

    feat: add global rate limit middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,5 +15,5 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/swipes", app.createSwipeHandler)
 
-	return app.recoverPanic(router)
+	return app.recoverPanic(app.rateLimit(router))
 }
