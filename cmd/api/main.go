6b4c7c1e52d9b944a43f9eec438388846d681928
thomas <thomas.treffry@nearform.com>
2024-07-08 15:40:31 +0100

    refactor: move serve functionality to it's own method

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -77,18 +77,7 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
-	}
-
-	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
-
-	err = srv.ListenAndServe()
+	err = app.serve()
 	logger.Error(err.Error())
 	os.Exit(1)
 }
