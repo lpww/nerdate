872c67c0deab148266eab4d5381e89dd2039861f
thomas <thomas.treffry@nearform.com>
2024-07-23 14:27:09 +0100

    feat: add user validation

--- internal/data/users.go
+++ internal/data/users.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/guregu/null"
+	"github.com/lpww/nerdate/internal/validator"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -52,4 +53,34 @@ func (p *password) Matches(plaintextPassword string) (bool, error) {
 	}
 
 	return true, nil
+}
+
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
+}
+
+func ValidatePasswordPlaintext(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) <= 72, "password", "must not exceed 72 bytes long")
+}
+
+func ValidateUser(v *validator.Validator, user *User) {
+	v.Check(user.Name != "", "name", "must be provided")
+	v.Check(len(user.Name) <= 500, "name", "must not exceed 500 bytes long")
+
+	v.Check(!user.DOB.IsZero(), "dob", "must be provided")
+
+	ValidateEmail(v, user.Email)
+
+	if user.Password.plaintext != nil {
+		ValidatePasswordPlaintext(v, *user.Password.plaintext)
+	}
+
+	// this is not a validation becuase it would not be caused by a user error
+	// it would only be triggered by a code error, so we panic
+	if user.Password.hash == nil {
+		panic("missing password hash for user")
+	}
 }
